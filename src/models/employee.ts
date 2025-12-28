e4413ac45535a8798f5d9f70430a7a63727ccc77
Lahiru Vimukthi <lahiruvimukthi292@gmail.com>
2025-12-28 20:37:41 +0930

    refactor: clean up .gitignore and add employee model interfaces

---
+++ src/models/employee.ts
@@ -0,0 +1,38 @@
+export interface Employee {
+  _id?: any;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
+  position: string;
+  image: string;
+}
+
+export interface Hotel {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  rating: number;
+  price: number;
+  amenities: string[];
+}
+
+export interface PackageFilters {
+  duration: string;
+  setDuration: (value: string) => void;
+  priceRange: string;
+  setPriceRange: (value: string) => void;
+}
+
+export interface EmployeeFormData {
+  id?: any;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
+  position: string;
+  image: string;
+}
