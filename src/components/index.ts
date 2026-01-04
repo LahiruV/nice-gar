1004f6d1bf5aa7e76c93ce7f451bda38547a9e66
Lahiru Vimukthi <lahiruvimukthi292@gmail.com>
2026-01-04 23:31:39 +0930

    feat: remove unused components and files including Header, Footer, LanguageSelector, and Excel model

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
-export { Header } from './Header';
-export { Footer } from './Footer';
 export { AuthCard } from './auth/AuthCard';
 export { LoginForm } from './auth/LoginForm';
 export { RegisterForm } from './auth/RegisterForm';
